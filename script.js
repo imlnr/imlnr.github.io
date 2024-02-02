c9ba46b6c2f0c981cfafbeea7e4580bf9bf9e2c9
imlnr <bablureddy553@gmail.com>
2024-02-02 19:35:12 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const edugit = document.getElementById("github");
+const edulive = document.getElementById("project1-git");
+
+const bangit = document.getElementById("github2");
+const banlive = document.getElementById("project2-git");
+
+const blogit = document.getElementById("github3");
+const blolive = document.getElementById("project3-git");
+
+
+
+edugit.addEventListener("click",()=>{
+    window.location.href = "https://github.com/imlnr/software-kalaakar-5678"
+});
+edulive.addEventListener("click",()=>{
+    window.location.href = "https://cw-p5.netlify.app/"
+})
+
+bangit.addEventListener("click",()=>{
+    window.location.href = "https://github.com/bsaha12/pixel-pioneer-7654"
+})
+banlive.addEventListener("click",()=>{
+    window.location.href = "https://pixel-pioneer-7654.netlify.app/"
+})
+
+blogit.addEventListener("click",()=>{
+    window.location.href = "https://github.com/anshukumari181405/overwought-skin"
+})
+blolive.addEventListener("click",()=>{
+    window.location.href = "https://software-kalaakar-5678.netlify.app/"
+})
