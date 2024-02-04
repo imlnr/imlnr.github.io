fbacae14a182b5254ffef14031eaa7e2af17ec2e
imlnr <bablureddy553@gmail.com>
2024-02-05 01:51:37 +0530

    final commit

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navigation-bar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
--- temp.js
+++ temp.js
@@ -1,28 +1,21 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.nav-bar-bottom');
-    if (window.scrollY > 50) {
-      navbar.classList.add('stic');
-    } else {
-      navbar.classList.remove('stic');
-    }
-  });
 
 
-  window.onscroll = function () {
-    scrollFunction();
+
+window.onscroll = function () {
+  scrollFunction();
 };
 
 function scrollFunction() {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
-    }
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 }
 
 // Scroll to top function
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 }
