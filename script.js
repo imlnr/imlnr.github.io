c01a88257bc4756ef65cb59dff10b73c1a549177
imlnr <bablureddy553@gmail.com>
2024-02-04 06:01:10 +0530

    done

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-const edugit = document.getElementById("github");
-const edulive = document.getElementById("project1-git");
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scroll functionality
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-const bangit = document.getElementById("github2");
-const banlive = document.getElementById("project2-git");
-
-const blogit = document.getElementById("github3");
-const blolive = document.getElementById("project3-git");
-
-function openInNewTab(url) {
-    window.open(url, '_blank');
-}
-
-edugit.addEventListener("click", () => {
-    openInNewTab("https://github.com/imlnr/software-kalaakar-5678")
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
-edulive.addEventListener("click", () => {
-    openInNewTab("https://cw-p5.netlify.app/")
-})
-
-bangit.addEventListener("click", () => {
-    openInNewTab("https://github.com/bsaha12/pixel-pioneer-7654")
-})
-banlive.addEventListener("click", () => {
-    openInNewTab("https://pixel-pioneer-7654.netlify.app/")
-})
-
-blogit.addEventListener("click", () => {
-    openInNewTab("https://github.com/anshukumari181405/overwought-skin")
-})
-blolive.addEventListener("click", () => {
-    openInNewTab("https://software-kalaakar-5678.netlify.app/")
-})
--- temp.js
+++ temp.js
@@ -6,3 +6,23 @@ window.addEventListener('scroll', function () {
       navbar.classList.remove('stic');
     }
   });
+
+
+  window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// Scroll to top function
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
